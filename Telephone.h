298ce4a8b4dfb4e92dc2a9429d84036745b7939e
Todd Olsen <todd.olsen@me.com>
2017-06-22 21:38:55 -0400

    Modified for cross-platform compatibility.

---
+++ Telephone.h
@@ -0,0 +1,16 @@
+//
+//  Telephone.h
+//  Telephone
+//
+//  Created by Todd Olsen on 6/22/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Telephone.
+FOUNDATION_EXPORT double TelephoneVersionNumber;
+
+//! Project version string for Telephone.
+FOUNDATION_EXPORT const unsigned char TelephoneVersionString[];
+
+
